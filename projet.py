bcbadd9741cb59fc23b4304b6c1d469401e63554
Achraf Jday <achraf.jd@gmail.com>
2020-11-23 10:23:54 -1200

    Update projet.py

--- projet.py
+++ projet.py
@@ -36,6 +36,7 @@ def getPrior(df):
     
     return result
 
+
 #Question2 :  programmation orientée objet dans la hiérarchie des Classifier
 
 class APrioriClassifier (utils.AbstractClassifier):
@@ -95,6 +96,7 @@ class APrioriClassifier (utils.AbstractClassifier):
         
         return dictionnaire_proba
     
+    
 #Question3 : classification probabiliste à 2 dimensions
 #Question3a :  probabilités conditionelles
     
@@ -151,6 +153,7 @@ def P2D_p(df, attr):
             dictionnaire_proba[cle][i] = dictionnaire_proba[cle][i] / taille
     return dictionnaire_proba
 
+
 #Question3b :  classifieurs 2D par maximum de vraisemblance
 
 class ML2DClassifier (APrioriClassifier):
@@ -187,6 +190,7 @@ class ML2DClassifier (APrioriClassifier):
             return 0
         return 1
     
+    
 #Question3c : classifieurs 2D par maximum a posteriori
 
 class MAP2DClassifier (APrioriClassifier):
@@ -222,6 +226,7 @@ class MAP2DClassifier (APrioriClassifier):
             #si la valeur de l'attribut n'existe pas dans l'ensemble d'apprentissage, sa probabilité conditionnelle n'est pas definie et on renvoie zero
             return 0
  
+    
 #Question4 :   
 #Question4.1 : complexité en mémoire
 
@@ -240,31 +245,32 @@ def nbParams(df, liste = None):
     taille = 8
     for col in liste:
         taille *= np.unique(df[col].values).size
-    res = str(len(liste)) + " variable(s) : " + str(taille) + " octets"
+    results = str(len(liste)) + " variable(s) : " + str(taille) + " octets"
     if taille >= 1024:
-        res = res + " = " + octetToStr(taille)
-    print (res)  
+        results = results + " = " + octetToStr(taille)
+    print (results)  
 
 def octetToStr(taille):
     """
-    Transforme l’entier taille en une chaîne de caractères qui donne sa représentation
+    Transforme l’entier taille en une chaîne de caractèresults qui donne sa représentation
     en nombre d’octets, ko, mo, go et to. 
     *parametre taille: le nombre à être transformé.
     """
     suffixe = ["o", "ko", "mo", "go", "to"]
-    res = ""
+    results = ""
     for suf in suffixe:
         if taille == 0:
             break  
         if suf == "to":
-            res = " {:d}".format(taille) + suf + res 
+            results = " {:d}".format(taille) + suf + results 
         else:
-            res = " {:d}".format(taille % 1024) + suf + res
+            results = " {:d}".format(taille % 1024) + suf + results
             taille //= 1024
     
-    if res == "":
-        res = " 0o"
-    return res[1:]
+    if results == "":
+        results = " 0o"
+    return results[1:]
+
 
 #Question4.2 : complexité en mémoire sous hypothèse d'indépendance complète
 
@@ -283,10 +289,10 @@ def nbParamsIndep(df):
     for col in liste:
         taille += (np.unique(df[col].values).size * 8)
     
-    res = str(len(liste)) + " variable(s) : " + str(taille) + " octets"
+    results = str(len(liste)) + " variable(s) : " + str(taille) + " octets"
     if taille >= 1024:
-        res = res + " = " + octetToStr(taille)
-    print (res)
+        results = results + " = " + octetToStr(taille)
+    print (results)
     
 
 #Question5 : Modèles graphiques
@@ -308,33 +314,34 @@ def drawNaiveBayes(df, col):
         resultant = resultant + col + "->" + child + ";"
     return utils.drawGraph(resultant[:-1])    
 
-def nbParamsNaiveBayes(df, col_pere, liste_col = None):
+def nbParamsNaiveBayes(df, col_father, liste_col = None):
     """
     Affiche la taille mémoire de tables P(target), P(attr1|target),.., P(attrk|target) 
-    étant donné un dataframe df, la colonne racine col_pere et la liste [target,attr1,...,attrk],
+    étant donné un dataframe df, la colonne racine col_father et la liste [target,attr1,...,attrk],
     en supposant qu'un float est représenté sur 8 octets. Pour cela cette fonction 
     utilise aussi la fonction auxiliaire octetToStr().
     *Les parametres:
     df: Dataframe contenant les données. 
-    col_pere: le nom de la colonne du Dataframe utilisée comme racine.
+    col_father: le nom de la colonne du Dataframe utilisée comme racine.
     liste_col: liste contenant les colonnes prises en considération pour le calcul. 
     """
-    taille = np.unique(df[col_pere].values).size * 8
+    taille = np.unique(df[col_father].values).size * 8
     
     if liste_col is None:
         liste_col = list(df.columns) 
         
     if liste_col != []:  
-        liste_col.remove(col_pere)
+        liste_col.remove(col_father)
     
     for col in liste_col:
-        temp = (np.unique(df[col_pere].values).size * np.unique(df[col].values).size) * 8
+        temp = (np.unique(df[col_father].values).size * np.unique(df[col].values).size) * 8
         taille += temp
     
-    res = str(len(liste_col)) + " variable(s) : " + str(taille) + " octets"
+    results = str(len(liste_col)) + " variable(s) : " + str(taille) + " octets"
     if taille >= 1024:
-        res = res + " = " + octetToStr(taille)
-    print (res)
+        results = results + " = " + octetToStr(taille)
+    print (results)
+ 
     
 #Question5.4 :  classifier naïve bayes
 
@@ -431,6 +438,7 @@ class MAPNaiveBayesClassifier(APrioriClassifier):
         P_1res = P_1 / (P_0 + P_1)
         return {0: P_0res, 1: P_1res}    
 
+
 #Question6 : feature selection dans le cadre du classifier naive bayes
         
 def isIndepFromTarget(df,attr,x):
@@ -502,10 +510,10 @@ class ReducedMLNaiveBayesClassifier(APrioriClassifier):
         """
         Construit un graphe orienté représentant naïve Bayes réduit.
         """
-        res = ""
+        results = ""
         for child in self.dictionnaire_P2D_l:
-            res = res + "target" + "->" + child + ";"
-        return utils.drawGraph(res[:-1])
+            results = results + "target" + "->" + child + ";"
+        return utils.drawGraph(results[:-1])
 
 class ReducedMAPNaiveBayesClassifier(APrioriClassifier):
     """
@@ -563,10 +571,11 @@ class ReducedMAPNaiveBayesClassifier(APrioriClassifier):
         """
         Construit un graphe orienté représentant naïve Bayes réduit.
         """
-        res = ""
+        results = ""
         for child in self.dictionnaire_P2D_l:
-            res = res + "target" + "->" + child + ";"
-        return utils.drawGraph(res[:-1])
+            results = results + "target" + "->" + child + ";"
+        return utils.drawGraph(results[:-1])
+
 
 #Question7 : évaluation des classifieurs
 #Question7.2 : 
@@ -596,6 +605,7 @@ def mapClassifiers(dic, df):
         ax.annotate(nom, (precision[i], rappel[i]))
     plt.show()
   
+    
 #Question8 : Sophistication du modèle (question BONUS)
 #Question8.1 : calcul des informations mutuelles
 
@@ -660,6 +670,7 @@ def ConditionalMutualInformation(df,x,y,z):
     matrix_res *= matrix_xyz
     return matrix_res.sum()
 
+
 ##Question8.2 : calcul de la matrice des poids
     
 def MeanForSymetricWeights(a):   
@@ -680,6 +691,7 @@ def SimplifyConditionalMutualInformationMatrix(a):
     """
     moy = MeanForSymetricWeights(a)
     a[a < moy] = 0
+
 
 #Question8.3 : Arbre (forêt) optimal entre les attributs
     
@@ -765,6 +777,233 @@ class Graphe:
  
 #Question8.4 : Orientation des arcs entre attributs
 
+def ConnexSets(list_arcs):
+    """
+    Costruit une liste des composantes connexes du graphe dont la liste d'aretes
+    est list_arcs.
+    *le parametre list_arcs: 
+    liste de triplets de la forme (sommet1, sommet2, poids).
+    *le return :
+    liste des composantes connexes du graphe dont la liste d'aretes
+    est list_arcs.
+    """
+    results = []
+    for (u, v, _) in list_arcs:
+        u_set = None
+        v_set = None
+        for s in results:
+            if u in s:
+                u_set = s
+            if v in s:
+                v_set = s
+        if u_set is None and v_set is None:
+            results.append({u, v})
+        elif u_set is None:
+            v_set.add(u)
+        elif v_set is None:
+            u_set.add(v)
+        elif u_set != v_set:
+            results.remove(u_set)
+            v_set = v_set.union(u_set)
+    return results
 
+def OrientConnexSets(df, arcs, classe):
+    """
+    Utilise l'information mutuelle (entre chaque attribut et la classe) pour
+    proposer pour chaque ensemble d'attributs connexes une racine et qui rend 
+    la liste des arcs orientés.
+    *les parametres:
+    df: le Dataframe. 
+    arcs: liste d'ensembles d'arcs connexes.
+    classe: colonne de réference dans le dataframe pour le calcul de l'information mutuelle.
+    *le return:
+     la liste des arcs orientés  
+    """
+    arcs_copy = arcs.copy()
+    list_sets = ConnexSets(arcs_copy)
+    list_arbre = []
+    for s in list_sets:
+        col_max = ""
+        i_max = -float("inf") #initialisation pour le max, il faut qu'il soit le plus petit possible au début.
+        for col in s:
+            i = MutualInformation(df, col, classe)
+            if i > i_max:
+                i_max = i
+                col_max = col
+        list_arbre += creeArbre(arcs_copy, col_max)
+    return list_arbre
+    
+def creeArbre(arcs, racine): 
+    """
+    À partir d'une liste d'arcs et d'une racine, renvoie l'arbre orienté depuis
+    cette racine. 
+    *les parametres:
+    arcs: liste d'ensembles d'arcs connexes.
+    racine: nom d'un sommet.
+    *le return:
+    l'arbre orienté.
+    """
+    results = []
+    file = [racine]
+    while file != []:
+        sommet = file.pop(0)
+        arcs_copy = arcs.copy()
+        for (u, v, poids) in arcs_copy:
+            if sommet == u:
+                results.append((u, v))
+                arcs.remove((u, v, poids))
+                file.append(v)
+            elif sommet == v:
+                results.append((v, u))
+                arcs.remove((u, v, poids))
+                file.append(u)
+    return results 
+
+#Question8.5 : Classifieur TAN 
+
+class MAPTANClassifier(APrioriClassifier):
+    """
+    Classifieur par le maximum a posteriori en utilisant le modèle TAN
+    (tree-augmented naïve Bayes).
+    """
+    def __init__(self, df):
+        """
+        Initialise le classifieur. 
+        *le parametre df: le dataframe.
+        """
+        self.createCmis(df)
+        arcs = Kruskal(df, self.cmis)
+        self.liste_arcs = OrientConnexSets(df, arcs, "target")
+        self.dictionnaire_P2D_l = {}
+        self.dictionnaire_P3D_l = {}
+        self.pTarget = {1: df["target"].mean()}
+        self.pTarget[0] = 1 - self.pTarget[1] 
+        tab_col = list(df.columns.values)
+        tab_col.remove("target")
+        for attr in tab_col:
+            father = self.is3D(attr)
+            if father is not False:
+                self.dictionnaire_P3D_l[attr] = P3D_l(df, attr, father)
+            else:
+                self.dictionnaire_P2D_l[attr] = P2D_l(df, attr)
+    def estimClass(self, attrs):
+        """
+        À partir d'un dictionanire d'attributs, estime la classe 0 ou 1.        
+        L'estimation est faite par maximum à posteriori à partir de dictionnaire_res.
+        *le parametre attrs:
+        le dictionnaire nom-valeur des attributs
+        *le return:
+        la classe 0 ou 1 estimée
+        """
+        dictionnaire_res = self.estimProbas(attrs)
+        if dictionnaire_res[0] >= dictionnaire_res[1]:
+            return 0
+        return 1
+        
+
+    def estimProbas(self, attrs):
+        """
+        Calcule la probabilité a posteriori P(target | attr1, ..., attrk) par
+        la méthode TAN (tree-augmented naïve Bayes).
+        *le parametre attrs:
+        le dictionnaire nom-valeur des attributs
+        *le return: 
+        la valeur de la proba.
+        """
+        P_0 = self.pTarget[0]
+        P_1 = self.pTarget[1]
+        for key in self.dictionnaire_P2D_l:
+            dictionnaire_p = self.dictionnaire_P2D_l[key]
+            if attrs[key] in dictionnaire_p[0]:
+                P_0 *= dictionnaire_p[0][attrs[key]]
+                P_1 *= dictionnaire_p[1][attrs[key]]
+            else:
+                return {0: 0.0, 1: 0.0}
+        for key in self.dictionnaire_P3D_l:
+            proba = self.dictionnaire_P3D_l[key]
+            P_0 *= proba.getProba(attrs[key], attrs[proba.father], 0)
+            P_1 *= proba.getProba(attrs[key], attrs[proba.father], 1)
+        if (P_0 + P_1) == 0 : 
+            return {0: 0.0, 1: 0.0}
+        P_0res = P_0 / (P_0 + P_1)
+        P_1res = P_1 / (P_0 + P_1)
+        return {0: P_0res, 1: P_1res}
+    
+    def draw(self):
+        """
+        Construit un graphe orienté représentant le modèle TAN.
+        """
+        results = ""
+        for child in self.dictionnaire_P2D_l:
+            results = results + "target" + "->" + child + ";"
+        for child in self.dictionnaire_P3D_l:
+            results = results + self.dictionnaire_P3D_l[child].father + "->" + child + ";"
+            results = results + "target" + "->" + child + ";"
+        return utils.drawGraph(results[:-1])
+    
+    def createCmis(self, df):
+        """
+        Crée la matrice de Conditional Mutual Information simplifiée à partir du dataframe df.
+        *le parametre df:
+        le dataframe. 
+        """
+        self.cmis = np.array([[0 if x == y else ConditionalMutualInformation(df, x, y, "target") 
+                                 for x in df.keys() if x != "target"] for y in df.keys() if y != "target"])
+        SimplifyConditionalMutualInformationMatrix(self.cmis)
+        
+    def is3D(self, attr):  
+        """
+        Détermine si l'attribut attr doit être représenté par une matrice 3D,
+        c'est-à-dire s'il a un parent outre que "target" dans self.list_arcs.
+        *le parametre attr:
+        nom d'un attribut du dataframe.
+        *le return: 
+        matrice3D ou non.
+        """
+        for father, fils in self.liste_arcs:
+            if fils == attr:
+                return father
+        return False
+    
+    
+class P3D_l():
+    """
+    Classe pour le calcul des probabilités du type P(attr1 | attr2, target).
+    """
+    def __init__(self, df, attr1, attr2):
+        """
+        *les parametres:
+        df: le dataframe. 
+        attr1: nom d'une colonne du dataframe.
+        attr2: nom d'une colonne du dataframe.
+        """
+        self.father = attr2
+        list_x = np.unique(df[attr1].values) 
+        list_y = np.unique(df[attr2].values) 
+        self.dictionnaire_x = {list_x[i]: i for i in range(list_x.size)}     
+        self.dictionnaire_y = {list_y[i]: i for i in range(list_y.size)} 
+        self.mat = np.zeros((list_x.size, list_y.size, 2))
+        group = df.groupby([attr1, attr2, 'target']).groups
+        for i, j, k in group:
+            self.mat[self.dictionnaire_x[i], self.dictionnaire_y[j], k] = len(group[(i, j, k)]) 
+        #normalisation de la matrice
+        quant = self.mat.sum(0)
+        quant[quant == 0] = 1 
+        self.mat = self.mat / quant.reshape((1, list_y.size, 2))
+        
+    def getProba(self, i, j, k):
+        """
+        Renvoie la valeur de P(attr1 = i | attr2 = j, target = k).
+        *les parametres:
+        i: valeur pour l'attribut attr1 de init.
+        j: valeur pour l'attribut attr2 de init.
+        k: valeur pour target.
+        """
+        if i in self.dictionnaire_x and j in self.dictionnaire_y:
+            return self.mat[self.dictionnaire_x[i], self.dictionnaire_y[j], k]
+        return 0.
+
+
+    
     
 
